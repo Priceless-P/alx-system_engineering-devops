9809c50d7bc5ddddfcfab12ed66c6f9b2503a4ff
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-02-15 23:02:58 +0100

    correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,56 +1,48 @@
 #!/usr/bin/python3
 """
-Recurse it!
-This function queries a list of all
-hot posts on a given Reddit subreddit.
+This function that queries the Reddit API recursively.
 """
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
+def count_words(subreddit, word_list, after='', word_count={}):
     """Queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit
     """
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "AppleWebKit:0x16.api.advanced:v1.0.0 \
-                       (by /u/JazzlikeFig4018)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    res = requests.get(url, headers=headers, params=params,
-                       allow_redirects=False)
-    try:
-        results = res.json()
-        if res.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
-
-    results = res.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-
-    for child in results.get("children"):
-        titles = child.get("data").get("title").lower().split()
+    if not word_count:
         for word in word_list:
-            if word.lower() in titles:
-                times = len([t for t in titles if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+            if word.lower() not in word_count:
+                word_count[word.lower()] = 0
 
     if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+        word_dict = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+        for word in word_dict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {"User-Agent": "AppleWebKit:0x16.api.advanced:v1.0.0 \
+                       (by /u/JazzlikeFig4018)"}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=header, params=params,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        hot_post = response.json()['data']['children']
+        after = response.json()['data']['after']
+        for post in hot_post:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in word_count.keys():
+                word_count[word] += lower.count(word)
+
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, after, word_count)
